e8a205c0a62a6742af6811e40b5681e04d3bea97
和祯_BT <2821721063@qq.com>
2022-11-06 14:17:15 +0800

    修了一部分bug，添加了页面加载动画和发牌动画和翻牌动画，把游戏准备阶段的各种判定也写好了捏

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ beginning();
 io.on('connection', (socket) => {//这块比较乱，已经尽量在简写了（笑）
   socket.on('join', (roomid,username,type) => {
     roomid+='';
-    hg(roomid,'host').then((host)=>{
+    hg(roomid,'host').then(async (host)=>{
       if (host==undefined) {//列表中某个房间突然被解散的情况
         io.in(socket.id).emit('reload','unexist_room');
       }
@@ -60,6 +60,10 @@ io.on('connection', (socket) => {//这块比较乱，已经尽量在简写了（
         }
         if (type!='normal') {//进入专房等于离开房间列表，人数减一
           amount('list','reduce');
+          let player1=await hg(roomid,'player1');
+          let player2=await hg(roomid,'player1');
+          let stats=await hg(roomid,'stats');
+          io.in(socket.id).emit('card','member',player1,player2,stats);
         }
       }
     });
@@ -74,11 +78,15 @@ io.on('connection', (socket) => {
           hg('hoster',socket.data.username).then((roomId)=>{
             roomId+='';
             amount('list','reduce');
-            redis.hSet(roomId,'host',socket.data.username);
-            redis.hSet(roomId,'amount','1');
-            redis.hSet(roomId,'observe',id);
-            redis.hSet(roomId,'ico',ico);
-            redis.hSet(roomId,'name',name);
+            redis.hSet(roomId,{
+              'host':socket.data.username,
+              'amount':'1',
+              'observe':id,
+              'ico':ico,
+              'name':name,
+              'stats':'waitting',
+              'nums':'1,2,3,4,5,6,7,8,9,10,11',
+            });
             rg('rooms').then((value)=>{
               if (roomId>value) {
                 rs('rooms',roomId);
@@ -102,6 +110,13 @@ io.on('connection', (socket) => {
         io.to(id).emit('room','close');
     }
   });
+  socket.on('card',(action)=>{
+    switch (action) {
+      case 'join':
+        card_join(socket.data.room,socket.data.username);
+        break;
+    }
+  })
   socket.on('message', (msg) => {
     io.in(socket.data.room).emit('message',socket.data.username+" : "+msg);
   });
@@ -124,17 +139,21 @@ server.listen(2053, () => {
 
 function source() {
   app.get('/psc', (req, res) => {
-    const num=Math.round(Math.random()*9);
+    const num=Math.round(Math.random()*5);//0开始，十个图，前六小，后四大
     res.sendFile(__dirname + '/psc/psc/psc ('+num+').webp');
   });
   app.use('/source', express.static('psc'));
 }
 function beginning() {
   io.emit('some event', { someProperty: 'some value', otherProperty: 'other value' });
-  redis.hSet('list','amount','0');
-  redis.hSet('list','host','host');
-  redis.hSet('main','amount','0');
-  redis.hSet('main','host','host');
+  redis.hSet('list',{
+    'host':'host',
+    'amount':'0',
+  });
+  redis.hSet('main',{
+    'host':'host',
+    'amount':'0',
+  });
   redis.setNX('rooms','0');
 }
 function room_getlist(userid) {
@@ -167,6 +186,36 @@ async function getid(id,username) {
     }
   })
   await redis.hSet('hoster',username,result[0]);
+}
+async function card_join(room,name) {
+  room+='';
+  var player1=await hg(room,'player1');
+  var player2=await hg(room,'player2');
+  if (player2==undefined) {
+    if (player1==undefined) {
+      await redis.hSet(room,'player1',name);
+      io.in(room).emit('card','join',name);
+    }
+    else {
+      let nn=Math.round(Math.random()+1);
+      let observe=await hg(room,'observe');
+      await redis.hSet(room,{
+        'player2':name,
+        'stats':nn,
+      });
+      let value=await hg(room,'nums');
+      result=value.split(',');
+      let i=Math.round(Math.random()*10);
+      let a=result[i];
+      result.splice(i,1);
+      i=Math.round(Math.random()*9);
+      let b=result[i];
+      result.splice(i,1);
+      io.in(room).emit('card','start',player1,name,observe,nn,a,b);
+      result=result.toString();
+      redis.hSet(room,'nums',result);
+    }
+  }
 }
 
 async function rs(key,value) {
